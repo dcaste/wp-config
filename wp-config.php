f7931c29714e764e40efe7e61bc1382b1bf4228f
Dax Castellón Meyrat <daxcastellon@gmail.com>
2019-01-25 21:00:14 -0600

    Added security and "other" options. Comments are also easier to understand.

--- wp-config.php
+++ wp-config.php
@@ -15,6 +15,10 @@ define( 'FORCE_SSL_ADMIN', true );
 define( 'WP_HTTP_BLOCK_EXTERNAL', true );
 define( 'WP_ACCESSIBLE_HOSTS', 'example.com,*.anotherexample.com' );
 
+// Override file permissions.
+define('FS_CHMOD_FILE', 0644);
+define('FS_CHMOD_DIR', 0755);
+
 /**
  * Debugging options.
  *
@@ -39,7 +43,7 @@ if ( current_user_can( 'administrator' ) ) {
  *
  */
 
-// Sets the autosave interval time.
+// Sets the autosave interval time in seconds.
 define( 'AUTOSAVE_INTERVAL', 160 );
 
 // Sets the amount of post revisions to be saved.
@@ -48,11 +52,28 @@ define( 'WP_POST_REVISIONS', 10 );
 // Disable post revisions completely.
 define( 'WP_POST_REVISIONS', false );
 
-// Change the amount of days to delete the trash posts.
+// Change the amount of days you want to keep the thrash before delete it comopletely.
 define( 'EMPTY_TRASH_DAYS', 10 );
+
+// Disable trash. Posts will be deleted permanently.
+define('EMPTY_TRASH_DAYS', 0 );
 
 // Adjust the amount of memory allocated for Wodpress. Use it if you don't have access to php.ini.
 define( 'WP_MEMORY_LIMIT', '128M' );
 
 // Adjust the maximum amount of memory allowed for Wodpress. Use it if you don't have access to php.ini.
 define( 'WP_MAX_MEMORY_LIMIT', '256M' );
+
+// Repair your database automatically. 
+// After adding this, you need to visit the following URL to optimize and repair WordPress database.
+// http://example.com/wp-admin/maint/repair.php 
+define('WP_ALLOW_REPAIR', true);
+
+/**
+ * Other options.
+ *
+ */
+
+// Set the WP home and Site URL.
+define('WP_HOME', 'http://www.example.com');
+define('WP_SITEURL', 'http://www.example.com');
