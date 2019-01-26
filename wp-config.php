678907ca068082a27b61b8af86adcb2b50b3d952
Dax Castellón Meyrat <daxcastellon@gmail.com>
2019-01-25 21:38:43 -0600

    Added several options
    - Disable image edit
    - Disable Cookie domain
    - WP Cron funcionalities
    - Disable file mods

--- wp-config.php
+++ wp-config.php
@@ -11,9 +11,12 @@ define( 'DISALLOW_FILE_EDIT', true );
 // Force SSL conection in admin panel.
 define( 'FORCE_SSL_ADMIN', true );
 
+// Prevents installing or updating themes, plugins and WP versions via admin panel.
+define( 'DISALLOW_FILE_MODS', true );
+
 // Block external requests and admit a list of hosts.
 define( 'WP_HTTP_BLOCK_EXTERNAL', true );
-define( 'WP_ACCESSIBLE_HOSTS', 'example.com,*.anotherexample.com' );
+define( 'WP_ACCESSIBLE_HOSTS', 'wordpress.org,*.wordpress.org' );
 
 // Override file permissions.
 define('FS_CHMOD_FILE', 0644);
@@ -30,7 +33,7 @@ define( 'WP_DEBUG_LOG', true );
 define( 'WP_DEBUG_DISPLAY', false );
 @ini_set( 'display_errors', 0 );
 
-// Diaplay the amount of database queries in the admin panel.
+// Display the amount of database queries in the admin panel.
 if ( current_user_can( 'administrator' ) ) {
   global $wpdb;
   echo '<pre>';
@@ -64,10 +67,21 @@ define( 'WP_MEMORY_LIMIT', '128M' );
 // Adjust the maximum amount of memory allowed for Wodpress. Use it if you don't have access to php.ini.
 define( 'WP_MAX_MEMORY_LIMIT', '256M' );
 
-// Repair your database automatically. 
-// After adding this, you need to visit the following URL to optimize and repair WordPress database.
-// http://example.com/wp-admin/maint/repair.php 
+/**
+* Repair your database automatically. 
+* After adding this, you need to visit the following URL to optimize and repair WordPress database.
+* http://example.com/wp-admin/maint/repair.php 
+*/
 define('WP_ALLOW_REPAIR', true);
+
+// Sets cookie domain. This will reduce unnecessary bytes required for the end user to download.
+define( 'COOKIE_DOMAIN', 'www.example.com' );
+
+// Disables CRON.
+define( 'DISABLE_WP_CRON', true );
+
+// Sets a CRON timeout. Make sure a cron process cannot run more than once every WP_CRON_LOCK_TIMEOUT seconds.
+define( 'WP_CRON_LOCK_TIMEOUT', 60 );
 
 /**
  * Other options.
@@ -77,3 +91,6 @@ define('WP_ALLOW_REPAIR', true);
 // Set the WP home and Site URL.
 define('WP_HOME', 'http://www.example.com');
 define('WP_SITEURL', 'http://www.example.com');
+
+// Keep only the original set of image files, not all edited versions.
+define( 'IMAGE_EDIT_OVERWRITE', true );
